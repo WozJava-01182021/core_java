ca5f6af8a250bc57f37b64f10d0ca6a95e30ea85
Rukshan Jonny Uddin <rukshan.uddin@gmail.com>
2021-01-20 16:42:04 -0500

    Initial commit

---
+++ Core Java/src/rukshanu/core_java/common/animal/mammals/Mammal.java
@@ -0,0 +1,7 @@
+package rukshanu.core_java.common.animal.mammals;
+
+import rukshanu.core_java.common.animal.Animal;
+
+public class Mammal extends Animal{
+
+}
---
+++ Core Java/src/rukshanu/core_java/lesson01/exercises/Cat.java
@@ -0,0 +1,9 @@
+package rukshanu.core_java.lesson01.exercises;
+
+public class Cat extends Animal {
+	
+	public Cat () {
+		type = "cat"; 
+	}
+
+}
