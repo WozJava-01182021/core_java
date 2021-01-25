b4e0f888ed24e782d81f2d741ef8586eac6fa945
Rukshan Jonny Uddin <rukshan.uddin@gmail.com>
2021-01-25 15:28:32 -0500

    Lesson 3 last handson

--- L07PracticeHandsOn/src/com/rukshanu/handson7/HandsOn.java
+++ L07PracticeHandsOn/src/com/rukshanu/handson7/HandsOn.java
@@ -3,8 +3,12 @@ package com.rukshanu.handson7;
 public class HandsOn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Employee me = new Employee("Rukshan", "Uddin", "Trilogy Education", 1);
+		System.out.println(me.toString());
+		Manager julian = new Manager("Julian", "Jimenez", "Trilogy Education", 5, "Central Grading Team Manager");
+		System.out.println(julian.toString());
+		Engineer xander = new Engineer("Xander", "Rapstine", "Trilogy Education", 10, "Senior Developer");
+		System.out.println(xander);
 	}
 
 }
--- L07PracticeHandsOn/src/com/rukshanu/handson7/Manager.java
+++ L07PracticeHandsOn/src/com/rukshanu/handson7/Manager.java
@@ -1,5 +1,32 @@
 package com.rukshanu.handson7;
 
-public class Manager {
+public class Manager extends Employee {
+	String jobTitle;
+	
+	
+	Manager(){
+		firstName = "";
+		lastName = "";
+		companyName = "";
+		yearsAtCompany = 0;
+		jobTitle = "";
+	}
+	Manager(String firstName, String lastName, String companyName, int yearsAtCompany, String jobTitle){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.companyName = companyName;
+		this.yearsAtCompany = yearsAtCompany;
+		this.jobTitle = jobTitle;
+	}
+	
+	public String getJobTitle() { return jobTitle; }
+	public void setJobTitle(String jobTitle) { this.jobTitle = jobTitle; }
+	
+	@Override
+	public String toString() {
+		String str = String.format("%s %s has worked at %s for %s year(s) as a(n) %s.", firstName, lastName, companyName, yearsAtCompany, jobTitle);
+		return str;
+	}
+
 
 }
